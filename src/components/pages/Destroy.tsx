1d408670f5355ea8bfe9dd4a7431dc341f97a9c8
Shahzaib Anwar <89007917+shoaib-blocksdev@users.noreply.github.com>
2024-11-21 16:01:33 +0500

    remove error

--- src/components/pages/Destroy.tsx
+++ src/components/pages/Destroy.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import { Button } from "../ui/button";
 import { Card } from "../ui/card";
 import { apiRequest } from "../api/Api";
